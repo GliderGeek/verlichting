e64be48ea601bb26747bafe38bec6e954bab2ba5
glidergeek <GliderGeek@users.noreply.github.com>
2022-12-03 15:39:33 +0100

    add new trip(2022), modify script

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ source for sailboat website, using flask for server side rendering of the pages
 ## Run locally
 - install dependencies inside virtualenv. inside app folder: `pip install -r requirements.txt`
 - run with flask. inside app folder: `flask run`
+
+## Add new track
+- run script `scripts/parse_gpx.py` with filename of gpx
+- add json outputs in new row in db
+- run locally
+- modify stops in db
--- scripts/parse_gpx.py
+++ scripts/parse_gpx.py
@@ -1,8 +1,17 @@
+"""
+This script take one commandline argument: filepath of gpx
+It outputs two files for using in trips db:
+- track.json
+- stops.json
+"""
+
 import json
 from xml.dom import minidom
+import sys
 
 # parse an xml file by name
-file = minidom.parse('Frankr21.gpx')
+filename = sys.argv[1]
+file = minidom.parse(filename)
 
 #use getElementsByTagName() to get tag
 trksegs = file.getElementsByTagName('gpx')[0].getElementsByTagName('trk')[0].getElementsByTagName('trkseg')
@@ -31,39 +40,3 @@ for i, index in enumerate(segment_indices):
 	stops.append({'index': index, 'name': 'stop%s' % i})
 with open('stops.json', 'w') as f:
 	json.dump(stops, f)
-
-
-
-
-# trkpts = trksegs[0].getElementsByTagName('trkpt')
-# print('lon:', float(trkpts[0].attributes['lon'].value))
-
-
-
-# for segment in trksegs
-
-# print(len(trksegs))
-
-
-
-# print(models)
-
-# # one specific item attribute
-# print('model #2 attribute:')
-# print(models[1].attributes['name'].value)
-
-# # all item attributes
-# print('\nAll attributes:')
-# for elem in models:
-#   print(elem.attributes['name'].value)
-
-# # one specific item's data
-# print('\nmodel #2 data:')
-# print(models[1].firstChild.data)
-# print(models[1].childNodes[0].data)
-
-# # all items data
-# print('\nAll model data:')
-# for elem in models:
-#   print(elem.firstChild.data)
-
