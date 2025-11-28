a589f76079b6459f1978144910c71a6da6ea4209
glidergeek <GliderGeek@users.noreply.github.com>
2025-11-28 16:22:59 +0100

    try different response

--- app/app.py
+++ app/app.py
@@ -253,6 +253,10 @@ def create_excel():
         f.write(request.data)
 
     with open('test.xlsx', 'rb') as f:
-        return send_file(f, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        return send_file(
+            f, 
+            as_attachment=True,
+            download_name='test.xlsx',
+            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     
     return 'unexpected'
